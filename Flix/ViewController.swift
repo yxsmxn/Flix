bbc4cac8827bce259fc39c2893e44c55980bb034
Yasmin <yasminxhernandez@yahoo.com>
2020-09-25 20:51:27 -0700

    Initial Commit

---
+++ Flix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Yasmin Hernandez on 9/25/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
